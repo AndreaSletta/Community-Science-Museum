e86befa1e4d96539af126b6a2aa2b88092b9869e
AndreaSletta <70203798+AndreaSletta@users.noreply.github.com>
2020-12-09 12:44:48 +0100

    final

--- styles/js.js
+++ styles/js.js
@@ -2,3 +2,25 @@
 
 let height = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${height}px`);
+
+
+
+
+// smooth scroll that also works on safari
+
+$(document).ready(function(){
+    $("a").on('click', function(event) {
+      if (this.hash !== "") {
+        event.preventDefault();
+        
+        var hash = this.hash;
+  
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+ 
+          window.location.hash = hash;
+        });
+      } 
+    });
+  });
